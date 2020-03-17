7ac2cbab1b8d68da07a8615eff1e5e1c535457f5
Evgen Bondarenko <ak.bondarenko.e.s@gmail.com>
2020-03-17 09:24:52 +0200

    harm carusel

--- city.js
+++ city.js
@@ -4,6 +4,8 @@ var point = {};
 var clickPoint = {};
 var mousedownPoint = {};
 var plane;
+var hram;
+var man;
 var shift = {dx:0, dy:0};
 var isDragging = false;
 const _360 = 2 * Math.PI;
@@ -13,8 +15,15 @@ loadResources();
 function loadResources() {
     plane = new Image();
     plane.src = "images/plane.png";
+    hram = new Image();
+    hram.src = "images/hram.png";
+    man = new Image();
+    man.src = "images/man.png";
     warmillSprite = new Image();
     warmillSprite.src = "images/warmillSprite.png";
+    karusel = new Image();
+    karusel.src = "images/karusel.png";
+    
     
 }
 window.requestAnimFrame = (function(){ 
@@ -95,8 +104,8 @@ function drawUnits()
 {  
     let dx = shift.dx;
     let dy = shift.dy;
-    let kadr = Math.floor(warmillEffect.ticks/6) % 10;
-   if(kadr < 9)
+    let kadr = Math.floor(warmillEffect.ticks/5) % 9;
+   if(kadr < 8)
    {         
     warmillEffect.ticks ++;
    }
@@ -106,6 +115,14 @@ function drawUnits()
     kadr = 0;
    }
 
-   let sx = 429*kadr;
-   ctx.drawImage(warmillSprite,sx,0, 429, 429, 500-dx, 200-dy, 300, 300);
+  // let sx = 429*kadr;
+  let sx = 800*kadr;
+   ctx.drawImage(man,sx,0, 800, 800, 50-dx, 100-dy, 300, 300);
+  // ctx.drawImage(warmillSprite,sx,0, 429, 429, 420-dx, 170-dy, 200, 200);
+  // ctx.drawImage(warmillSprite,sx,0, 429, 429, 470-dx, 200-dy, 200, 200);
+   //ctx.drawImage(warmillSprite,sx,0, 429, 429, 520-dx, 250-dy, 200, 200); karusel
+
+   ctx.drawImage(hram,0,0, 700, 450, 300-dx, 300-dy, 280, 200);
+   ctx.drawImage(karusel,0,0, 460, 460, 220-dx, 220-dy, 200, 200);
+
 }
